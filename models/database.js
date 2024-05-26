94a06795366ef6f4f5db2983c82355528fb35154
LukasG62 <lukas.grando.62@gmail.com>
2024-05-26 17:25:53 +1200

    move database.js into model

--- models/database.js
+++ models/database.js
@@ -1,8 +1,11 @@
 import { Sequelize } from 'sequelize';
 import config from '../config/config.js';
-import logger from './logger.js';
+import logger from '../utils/logger.js';
+import initModels from './init-models.js';
 
 const mysql = config.mysql;
+
+let models = {};
 
 const db = new Sequelize(mysql.database, mysql.user, mysql.password, {
     host: mysql.host,
@@ -13,13 +16,18 @@ const db = new Sequelize(mysql.database, mysql.user, mysql.password, {
     logging: (msg) => logger.info(`[SEQUELIZE]: ${msg}`),
 });
 
+
 // Test the connection
 db.authenticate()
     .then(() => {
         logger.success('Connection has been established successfully.');
+        models = initModels(db);
+        logger.success('Models have been initialized successfully.');
     })
     .catch((error) => {
         logger.error(`${error}`);
     });
 
-export default db;
+
+// Export the database connection and models
+export { db, models };
